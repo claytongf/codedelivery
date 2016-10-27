7cfcdb8639b6f24e43106f88073469999a7a91da
Clayton Figueiredo <clayton@techwing.com.br>
2016-10-27 19:14:00 -0200

    Entrega 1

---
+++ app/Services/ClientService.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Clayton
+ * Date: 27/10/2016
+ * Time: 17:38
+ */
+
+namespace CodeDelivery\Services;
+
+
+use CodeDelivery\Repositories\ClientRepository;
+use CodeDelivery\Repositories\UserRepository;
+
+class ClientService
+{
+    /**
+     * @var ClientRepository
+     */
+    private $clientRepository;
+    /**
+     * @var UserRepository
+     */
+    private $userRepository;
+
+    public function __construct(ClientRepository $clientRepository, UserRepository $userRepository)
+    {
+        $this->clientRepository = $clientRepository;
+        $this->userRepository = $userRepository;
+    }
+
+    public function update(array $data, $id){
+        $this->clientRepository->update($data, $id);
+        $userId = $this->clientRepository->find($id, ['user_id'])->user_id;
+        $this->userRepository->update($data['user'], $userId);
+    }
+
+    public function create(array $data){
+        $data['user']['password'] = bcrypt(123456);
+        $user = $this->userRepository->create($data['user']);
+        $data['user_id'] = $user->id;
+        $this->clientRepository->create($data);
+    }
+}
